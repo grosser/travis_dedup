0ec990194bdfc3a165cc4a3aace5e11332a9c888
grosser <michael@grosser.it>
2015-03-03 22:41:43 -0800

    do not log tokens

--- server/app.rb
+++ server/app.rb
@@ -1,6 +1,16 @@
 require 'sinatra'
 require 'travis_dedup'
 
+# sinatra logs to stderr which is an IO
+# but we don't want to store other peoples token
+module NoTokenLogging
+  def write(*args)
+    args.first.sub!(/token=\S+/, "token=[FILTERED]")
+    super
+  end
+end
+IO.prepend NoTokenLogging
+
 post "/github" do
   TravisDedup.pro = params["pro"]
   TravisDedup.dedup_message(params.fetch("repo"), params.fetch("token"))
