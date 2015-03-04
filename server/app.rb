5de2dc3dc187549ce1fb7bae90f2241e412773a7
grosser <michael@grosser.it>
2015-03-04 06:50:29 -0800

    add hello message for root

--- server/app.rb
+++ server/app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'travis_dedup'
+require 'travis_dedup/version'
 
 # sinatra logs to stderr which is an IO
 # but we don't want to store other peoples token
@@ -10,6 +11,10 @@ module NoTokenLogging
   end
 end
 IO.prepend NoTokenLogging
+
+get "/" do
+  "Welcome to travis-dedup version #{TravisDedup::VERSION}"
+end
 
 post "/github" do
   TravisDedup.pro = params["pro"]
